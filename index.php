c37ed21b0a8bcd8c66bb075ea0b3b8c050b57354
Zombie-Muse <gjmccormack@cpp.edu>
2020-10-15 19:15:18 -0700

    more files

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Request broker GuitarShop Application.
+ * 
+ * @author jam
+ * @version 180428
+ */
+// Non-web tree base directory for this application.
+define('NON_WEB_BASE_DIR', 'H:/ArtHub/');
+define('APP_NON_WEB_BASE_DIR', NON_WEB_BASE_DIR . 'ArtHub/');
+include_once(APP_NON_WEB_BASE_DIR . 'includes/guitarShopIncludes.php');
+
+// Sanitze the routing input from links and forms - set default values if
+// missing.
+$post = true;
+if (hRequestMethod() === 'GET') {
+    $vm = null;
+    $actionGET = hGET('action');
+    $ctlrGET = hGET('ctlr');
+    $ctlr = isset($ctlrGET) ? $ctlrGET : '';
+    $actionSet = isset($actionGET) ? $actionGET : '';
+
+    // Whitelist actions from a GET request.
+    $action = hasInclusionIn($actionSet, $whiltelistGET) ? $actionSet : '';
+    if (!$action !== '') {
+        $post = false;
+    }
+} else {
+
+    // POST request processing
+    $vm = MessageVM::getErrorInstance();
+
+    $actionPost = hPOST('action');
+    $ctlrPost = hPOST('ctlr');
+    $action = isset($actionPost) ? $actionPost : '';
+    $ctlr = isset($ctlrPost) ? $ctlrPost : 'index';
+
+    if ($vm->errorMsg !== '') {
+        $action = '';
+        $ctlr = '';
+    }
+}
+
+switch ($ctlr) {
+    case 'admin':
+        $controller = new AdminController();
+        if ($action === 'login') {
+            if ($post) {
+                $action = 'loginPOST';
+            } else {
+                $action = 'loginGET';
+            }
+        }
+        if ($action === 'addProduct') {
+            if ($post) {
+                $action = 'addEditProduct';
+            } else {
+                $action = 'showAddProduct';
+            }
+        }
+        break;
+    case 'home':
+        $controller = new HomeController();
+        break;
+    case 'cart':
+        $controller = new CartController();
+        break;
+    default:
+        $controller = new DefaultController();
+}
+$controller->run($action, $vm);
